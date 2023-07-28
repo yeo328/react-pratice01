2f67145c99c6a954845ac32570c74076ea60a742
yeo <yeo328@kakao.com>
2023-07-28 10:30:52 +0900

    0728 test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './css/App.css';
+import {HashRouter, Route} from 'react-router-dom';
+import Skills from './routes/Skills.js';
+import Home from './routes/Home.js';
+import Practice from './routes/Practice.js'
+import Navigation from './components/Navigation.js';
+import Top from './components/Top.js';
 
-function App() {
+import TripP from './routes/TripP';
+import Count from './routes/Count.js';
+import Project from './routes/Project';
+import Matzip from './components/Matzip';
+import History from './routes/History.js';
+
+function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HashRouter>
+        <Navigation />
+        <Top />
+        <Route path="/practice"  component={Home}/>
+        <Route path="/skills" component={Skills}/>
+        <Route path="/before" component={Practice}/>
+        <Route path="/trip" component={TripP}/>
+        <Route path="/" exact={true} component={Count}/>
+        <Route path="/project" component={Project}/>
+        <Route path="/matzip" component={Matzip}/>
+        <Route path="/history" component={History}/>
+    </HashRouter>
   );
 }
+
 
 export default App;
